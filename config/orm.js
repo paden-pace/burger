9437f8929cc474142a0169a492d3f9f7222135e2
Paden Pace <pjpace27@yahoo.com>
2017-04-23 21:45:23 -0500

    addition of config folder with basic config files

---
+++ config/orm.js
@@ -0,0 +1,28 @@
+var connection = require("./connection");
+
+
+var orm = {
+    selectAll: function(whatToSelect, table, orderCol, orderBy){
+        var queryString = "SELECT ?? FROM ?? ORDER BY ? ?";
+        console.log(queryString);
+        connection.query(queryString, [whatToSelect, table, orderCol, orderBy], function(err, result) {
+            console.log(results);
+        });
+    }, 
+    insertOne: function(tableToIns, colOne, colTwo, valOne, valTwo){
+        var queryString = "INSERT INTO ?? (??, ??) VALUES (??, ?)";
+        console.log(queryString);
+        connection.query(queryString, [tableToIns, colOne, colTwo, valOne, valTwo], function(err, result) {
+            console.log(results);
+        });
+    }, 
+    updateOne: function(){
+        var queryString = "UPDATE ?? SET ?? = ?? WHERE ?? = ??";
+        console.log(queryString);
+        connection.query(queryString, [tableToUp, setCol, setVal, whereCol, whereVal], function(err, result) {
+            console.log(results);
+        });
+    }
+};
+
+module.exports = orm;
